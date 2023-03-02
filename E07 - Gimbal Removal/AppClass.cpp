84cf6c0a714b53295fbfa743a2f5200c700ea1e9
nrb4824 <nrb4824@rit.edu>
2023-03-02 10:28:57 -0500

    extra

--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -34,13 +34,20 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
+	/*quaternion q1 = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	quaternion q2 = glm::angleAxis(glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
+	quaternion q3 = glm::angleAxis(glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
+	quaternion q4 = q1 * q2;
+	q4 = glm::cross(q4, q3);*/
+
 	m_m4Model = glm::toMat4(m_qOrientation);
 
 	/*
 	* The following line was replaced by the model manager so we can see a model instead of a cone
 	*/
 	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
-	m_pModelMngr->AddModelToRenderList(m_sSteve,m_m4Model);
+	m_pModelMngr->AddModelToRenderList(m_sSteve, m_m4Model);
+	/*m_pModelMngr->AddModelToRenderList(m_sSteve,ToMatrix4(q4));*/
 
 	// draw a skybox
 	m_pModelMngr->AddSkyboxToRenderList();
